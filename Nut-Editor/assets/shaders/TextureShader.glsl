2575593542af5c669eb4813f3f83bd181da4ed81
JJJJJJJustin <2485400194@qq.com>
2025-02-16 22:33:54 +0800

    93.1:Fixed texture bug（修复纹理的错误）

--- Nut-Editor/assets/shaders/TextureShader.glsl
+++ Nut-Editor/assets/shaders/TextureShader.glsl
@@ -20,18 +20,18 @@ struct VertexOutput
 {
 	vec4  Color;
 	vec2  TexCoord;
-	float TexIndex;
 	float TilingFactor;
 };
 
 layout(location = 0) out VertexOutput Output;
+layout(location = 3) out flat float v_TexIndex;
 layout(location = 4) out flat int v_EntityID;
 
 void main()
 {
 	Output.Color = a_Color;
 	Output.TexCoord = a_TexCoord;
-	Output.TexIndex = a_TexIndex;
+	v_TexIndex = a_TexIndex;
 	Output.TilingFactor = a_TilingFactor;
 	v_EntityID = a_EntityID;
 
@@ -47,11 +47,11 @@ struct VertexOutput
 {
 	vec4  Color;
 	vec2  TexCoord;
-	float TexIndex;
 	float TilingFactor;
 };
 
 layout (location = 0) in VertexOutput Input;
+layout (location = 3) in flat float v_TexIndex;
 layout (location = 4) in flat int v_EntityID;
 
 layout (binding = 0) uniform sampler2D u_Textures[32];
@@ -61,7 +61,7 @@ layout (location = 1) out int color2;
 
 void main()
 {
-	color = texture(u_Textures[int(Input.TexIndex)], Input.TexCoord * Input.TilingFactor) * Input.Color;
+	color = texture(u_Textures[int(v_TexIndex)], Input.TexCoord * Input.TilingFactor) * Input.Color;
 	
 	color2 = v_EntityID;
 }
