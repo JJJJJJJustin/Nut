f713086cde8ce43bf0c7afcc9e600e570e065153
JJJJJJJustin <2485400194@qq.com>
2025-02-16 22:46:45 +0800

    93.2:Changed the filter mode（改变滤波模式）

--- Nut/src/Platform/OpenGL/OpenGLTexture.cpp
+++ Nut/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -19,7 +19,7 @@ namespace Nut
 		glTextureStorage2D(m_RendererID, 1, m_InternalFormat, m_Width, m_Height);			// 储存数据(width 和 height 表示像素点个数形成的宽与高）
 
 		glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);				//纹理过滤
-		glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);					//纹理环绕模式
 		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	}
@@ -58,7 +58,7 @@ namespace Nut
 		glTextureStorage2D(m_RendererID, 1, internalFormat, m_Width, m_Height);				// 储存数据
 
 		glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);				//纹理过滤（根据情况修改）
-		glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);					//纹理环绕模式
 		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
